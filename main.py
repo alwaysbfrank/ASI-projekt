ea261e6107ec3cad991cb46fdabf8419b5588b65
unknown <dominikspiewak97@gmail.com>
2022-06-01 22:33:03 +0200

    add model monitoring

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 
 # Press the green button in the gutter to run the script.
-from model import readModel, trainModel, evaluateModel
+from model import readModel, trainModel, evaluateModel, monitoringModel
 
 if __name__ == '__main__':
     readModel.read()
     trainModel.train()
     evaluateModel.evaluate()
+    monitoringModel.monitoring()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
