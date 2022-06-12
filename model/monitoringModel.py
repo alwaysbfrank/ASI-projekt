631e59d8a564b7bab227462a404147bb7306dfd6
unknown <dominikspiewak97@gmail.com>
2022-06-12 16:32:56 +0200

    monitoring: add loading only newest pkl file and saving to csv with loaded file timestamp

--- model/monitoringModel.py
+++ model/monitoringModel.py
@@ -4,11 +4,19 @@ import pickle
 from sklearn.metrics import mean_squared_error, r2_score
 from datetime import datetime
 import os.path
+import glob
 
 
 def monitoring():
     # Load model
-    model = pickle.load(open("model/model_1.2.pkl", 'rb'))
+    # model = pickle.load(open("model/model_2022-12-06_15-47-48.pkl", 'rb'))
+
+    # load only last created .pkl file
+    list_of_files = glob.glob('model/*.pkl', recursive=True)
+    latest_file = max(list_of_files, key=os.path.getctime)
+    last_file_timestamp = latest_file.split('model_')[1].split('.pkl')[0]
+    model = pickle.load(open(latest_file, 'rb'))
+
 
     # Read test data
     batch_no = 4
@@ -35,10 +43,10 @@ def monitoring():
     eval_df = eval_df.append({'time_stamp':now, 'version': '1.0', 'batch': batch_no, 'metric': 'r2', 'score': r2}, ignore_index=True)
 
     # Save evaluation to file
-    evaluation_file_name = 'data/model_eval.csv'
+    evaluation_file_name = 'data/model_eval_' + last_file_timestamp + '.csv'
 
     if os.path.isfile(evaluation_file_name):
-        eval_df.to_csv('data/model_eval.csv', mode='a', index=False, header=False)
+        eval_df.to_csv(evaluation_file_name, mode='a', index=False, header=False)
     else:
-        eval_df.to_csv('data/model_eval.csv', index=False)
+        eval_df.to_csv(evaluation_file_name, index=False)
     return batch_no
